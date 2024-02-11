d8e80ec517e9ee8fdee6663d0bfe08e1bd3fe924
Mhjba <mahjoubaaitalla15@gmail.com>
2024-02-11 14:00:14 +0100

    airbnb

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,85 +2,64 @@
 """ Contains unit tests for class BaseModel """
 
 import unittest
-import sys
 import os
-from datetime import datetime
 from models.base_model import BaseModel
-import models
-
 
 class TestBaseModel(unittest.TestCase):
-    """test for class BaseModel and its methods
-    """
+    """Test for class BaseModel"""
+
     def setUp(self):
-        """ Set up method
-        """
+        """Set up method"""
+
         self.model = BaseModel()
-        self.modl = BaseModel()
 
     def tearDown(self):
         """
         Resets tests
         """
-        try:
+        if os.path.exists("file.json"):
             os.remove("file.json")
-        except:
-            pass
 
-    def test_uuid(self):
-        """ Test for uuid
-        """
-        self.assertNotEqual(self.model.id, self.modl.id)
-        self.assertTrue(hasattr(self.model, "id"))
-        self.assertEqual(type(self.model.id), str)
-        self.assertEqual(type(self.modl.id), str)
-
-    def test_ins(self):
-        """ Test for instance
-        """
-        self.assertTrue(isinstance(self.model, BaseModel))
-        self.assertTrue(isinstance(self.modl, BaseModel))
-
-    def test_save(self):
-        """ Test for save
-        """
-        cls= self.model.updated_at
-        self.model.save()
-        self.assertFalse(cls == self.model.updated_at)
-
-    def test_update(self):
-        """ Test for update
+    def test_init_kw(self):
+        """Test to initialize with kwargs
         """
         self.model.name = "Holberton"
-        self.assertTrue(hasattr(self.model, "name"))
-
-    def test_kw(self):
-        """ Test to init for kwargs
-        """
-        self.model.name = "Holberton"
-        model_json = self.model.to_dict()
-        new_model = BaseModel(**model_json)
-        self.assertDictEqual(model_json, new_model.to_dict())
+        model_dict = self.model.to_dict()
+        new_model = BaseModel(**model_dict)
+        self.assertDictEqual(model_dict, new_model.to_dict())
         self.assertIn("name", new_model.to_dict())
         self.assertIsNot(self.model, new_model)
 
-    def test_sto(self):
-        """ Test to staorage
-        """
-        obj_dict = models.storage.all()
-        self.assertTrue(obj_dict)
+    def test_str(self):
+        """Checking correct output when printing"""
 
-    def test_docstr(self):
-        """ test doc in the file
-        """
-        self.assertIsNotNone(BaseModel.__doc__)
-        self.assertIsNotNone(BaseModel.__init__.__doc__)
-        self.assertIsNotNone(BaseModel.save.__doc__)
-    
-    def test__str__(self):
-        """[Cheking correct output when printing]"""
-        cls = self.model.id
-        self.assertTrue(f'[BaseModel] ({cls})' in str(self.model))
+        cls_id = self.model.id
+        self.assertIn(f'[BaseModel] ({cls_id})', str(self.model))
+
+    def test_save(self):
+        """Checks if updated_at is changed with the save method"""
+
+        self.model.save()
+        self.assertNotEqual(self.model.updated_at, self.model.created_at)
+
+    def test_save_method(self):
+        """Checks if the generated key is saved in the json file"""
+
+        obj = BaseModel()
+        obj.save()
+        key_id = f"BaseModel.{obj.id}"
+        with open("file.json", mode="r", encoding="utf-8") as file:
+            self.assertIn(key_id, file.read())
+
+    def test_to_dict(self):
+        """Checks to_dict method returns a dictionary"""
+
+        cls = BaseModel()
+        dict_to = cls.to_dict()
+        self.assertIsInstance(dict_to, dict)
+        self.assertIsInstance(dict_to['created_at'], str)
+        self.assertIsInstance(dict_to['updated_at'], str)
 
 if __name__ == '__main__':
     unittest.main()
+
