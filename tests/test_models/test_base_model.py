271204cdba8e3bc7d11232cc7d116e0a88e1841f
Mhjba <mahjoubaaitalla15@gmail.com>
2024-02-11 13:23:40 +0100

    airbnb

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,105 +1,86 @@
 #!/usr/bin/python3
-"""Unittest for BaseModel"""
+""" Contains unit tests for class BaseModel """
 
-from contextlib import redirect_stdout
 import unittest
-from models.base_model import BaseModel
-from datetime import datetime
-import io
-import os
 import sys
+import os
+from datetime import datetime
+from models.base_model import BaseModel
+import models
 
 
-class baseTest(unittest.TestCase):
-    """Class that tests BaseModel"""
+class TestBaseModel(unittest.TestCase):
+    """test for class BaseModel and its methods
+    """
+    def setUp(self):
+        """ Set up method
+        """
+        self.model = BaseModel()
+        self.modl = BaseModel()
 
-    def test_init(self):
-        """test initialisation"""
-        model = BaseModel()
-        model.name = "Test"
-        self.assertEqual(model.name, 'Test')
+    def tearDown(self):
+        """
+        Resets tests
+        """
+        try:
+            os.remove("file.json")
+        except:
+            pass
 
-    def test_init2(self):
-        """test initialisation"""
-        model = BaseModel()
-        model.name = "Test"
-        model.my_number = 29
-        self.assertEqual(model.name, "Test")
-        self.assertEqual(model.my_number, 29)
+    def test_uuid(self):
+        """ Test for uuid
+        """
+        self.assertNotEqual(self.model.id, self.modl.id)
+        self.assertTrue(hasattr(self.model, "id"))
+        self.assertEqual(type(self.model.id), str)
+        self.assertEqual(type(self.modl.id), str)
 
-    def test_initkwargs(self):
-        """test init with kwargs"""
-        model = BaseModel(name='Test', my_number=30)
-        self.assertEqual(model.name, 'Test')
-        self.assertEqual(model.my_number, 30)
-
-    def test_initid(self):
-        """test allocation of uuid"""
-        model = BaseModel()
-        self.assertEqual(type(model.id), str)
-        self.assertEqual(len(model.id), 36)
-
-    def test_initdate(self):
-        """test datetime"""
-        model = BaseModel()
-        x = str(datetime.now())[:-10]
-        y = str(model.created_at)[:-10]
-        z = str(model.updated_at)[:-10]
-        self.assertEqual(x, y)
-
-    def test_str(self):
-        """test __str__"""
-        model = BaseModel()
-        model.name = "Test"
-        model.my_number = 29
-        output = ""
-        with io.StringIO() as buf, redirect_stdout(buf):
-            print(model)
-            output = buf.getvalue()
-        self.assertEqual(output[1:10], 'BaseModel')
-        z = '\'my_number\': 29'
-        x = z in output
-        self.assertEqual(x, True)
+    def test_ins(self):
+        """ Test for instance
+        """
+        self.assertTrue(isinstance(self.model, BaseModel))
+        self.assertTrue(isinstance(self.modl, BaseModel))
 
     def test_save(self):
-        """test update attr after/during save"""
-        model = BaseModel()
-        x = model.updated_at
-        model.name = "Test"
-        model.save()
-        self.assertNotEqual(x, model.updated_at)
-        self.assertEqual(str(x)[:-10], str(model.updated_at)[:-10])
+        """ Test for save
+        """
+        cls= self.model.updated_at
+        self.model.save()
+        self.assertFalse(cls == self.model.updated_at)
 
-    def test_todict(self):
-        """test to_dict object function"""
-        model = BaseModel()
-        x = model.to_dict()
-        self.assertEqual('id' in x.keys(), True)
-        self.assertEqual('created_at' in x.keys(), True)
-        self.assertEqual(type(x.get('created_at')), str)
+    def test_update(self):
+        """ Test for update
+        """
+        self.model.name = "Holberton"
+        self.assertTrue(hasattr(self.model, "name"))
 
-    def test_todict2(self):
-        """test to_dict with new attributes"""
-        model = BaseModel()
-        model.name = "Test"
-        x = model.to_dict()
-        self.assertEqual('name' in x.keys(), True)
-        self.assertEqual('number' in x.keys(), False)
-        model.number = 29
-        x = model.to_dict()
-        self.assertEqual('number' in x.keys(), True)
+    def test_kw(self):
+        """ Test to init for kwargs
+        """
+        self.model.name = "Holberton"
+        model_json = self.model.to_dict()
+        new_model = BaseModel(**model_json)
+        self.assertDictEqual(model_json, new_model.to_dict())
+        self.assertIn("name", new_model.to_dict())
+        self.assertIsNot(self.model, new_model)
 
-    def test_modelfromdict(self):
-        """test creating basemodel from dict"""
-        model = BaseModel()
-        model.name = "Test"
-        x = model.to_dict()
-        self.assertEqual('number' in x.keys(), False)
-        self.assertEqual('name' in x.keys(), True)
-        model2 = BaseModel(**x)
-        self.assertEqual(model2.name, 'Test')
-        self.assertEqual(model2.created_at, model.created_at)
+    def test_sto(self):
+        """ Test to staorage
+        """
+        obj_dict = models.storage.all()
+        self.assertTrue(obj_dict)
 
+    def test_docstr(self):
+        """ test doc in the file
+        """
+        self.assertIsNotNone(BaseModel.__doc__)
+        self.assertIsNotNone(BaseModel.__init__.__doc__)
+        self.assertIsNotNone(BaseModel.save.__doc__)
+    
+    def test__str__(self):
+        """[Cheking correct output when printing]"""
+        cls = self.model.id
+        self.assertTrue(f'[BaseModel] ({cls})' in str(self.model))
 
 if __name__ == '__main__':
     unittest.main()
